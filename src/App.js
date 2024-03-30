d539de89e89cf87500521abf147add30cfefc6fa
Kirill Goshin <4kirillgo@gmail.com>
2024-03-30 21:20:18 +0300

    oyunun basit hali bitti

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const DeveCuceOyunu = () => {
+  const [target, setTarget] = useState(""); // Hedef (Deve veya Cüce)
+  const [score, setScore] = useState(0); // Puan
+  const [userInput, setUserInput] = useState(""); // Kullanıcının girişi
+  const [color, changeColor] = useState("");
+  // Her 2 saniyede bir yeni hedef belirleme
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTarget(Math.random() < 0.5 ? "Deve" : "Cüce");
+      const deve = document.querySelector('#deve');
+      const cuce = document.querySelector('#cuce');
+      deve.disabled = false;
+      cuce.disabled = false;
+      changeColor(randomColor());
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function randomColor() {
+    let rb = Math.floor(Math.random() * 255);
+    let rr = Math.floor(Math.random() * 255);
+    let rg = Math.floor(Math.random() * 255);
+
+    return `rgb(${rr}, ${rg}, ${rb}, 0.5)`
+  }
+  // Kullanıcının girişini kontrol etme
+  const handleClick = (event) => {
+    const input = event.target.dataset.name;
+    if (input === "Deve" || input === "Cüce") {
+      setUserInput(input);
+      const deve = document.querySelector('#deve');
+      const cuce = document.querySelector('#cuce');
+      deve.disabled = true;
+      cuce.disabled = true;
+    }
+  };
+
+  // Her kullanıcı girişi için kontrol yapma
+  useEffect(() => {
+    if (userInput === "") return;
+
+    if (userInput === target) {
+      setScore(score + 1);
+    } else {
+      setScore(score - 1);
+    }
+    setUserInput("");
+  }, [userInput]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-3 p-3  w-50" style={{backgroundColor: color}}>
+      <h1>Deve Cüce Oyunu</h1>
+      <p id="hedef">Hedef: {target}</p>
+      <p>Puan: {score}</p>
+      <button id="deve" data-name="Deve" className="btn btn-secondary m-2" onClick={handleClick} >Deve</button>
+      <button id="cuce" data-name="Cüce" className="btn btn-secondary" onClick={handleClick}>Cüce</button>
     </div>
   );
-}
+};
 
-export default App;
+export default DeveCuceOyunu;
